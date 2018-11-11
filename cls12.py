7fed44d91a68953f0a6dc3786a4613071a34c3b8
luyu2995 <18817572995@163.com>
2018-11-11 19:45:02 +0800

    add 12

---
+++ cls12.py
@@ -0,0 +1,36 @@
+
+def taiyi(radius, color1, color2):
+    t.color('black', color1)
+    t.begin_fill()
+    t.circle(radius/2, 180)
+    t.circle(radius, 180)
+    t.left(180)
+    t.circle(-radius/2, 180)
+    t.end_fill()
+
+    t.left(90)
+    t.penup()
+    t.fd(radius*0.35)
+    t.right(90)
+    t.down()
+    t.color(color1, color2)
+
+    t.begin_fill()
+    t.circle(radius*0.15)
+    t.end_fill()
+
+    t.left(90)
+    t.up()
+    t.bk(radius*0.35)
+    t.down()
+    t.left(90)
+def Main():
+    taiyi(200, 'black', 'white')
+    taiyi(200, 'white', 'black')
+
+
+
+import turtle
+t = turtle.Pen()
+Main()
+turtle.mainloop()
