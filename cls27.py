1f0b0e67695334abc5e38ced32a1288d5af0cf3f
liamlycoder <luyu.real@qq.com>
2019-02-15 15:38:03 +0800

    modify somthing

--- cls27.py
+++ cls27.py
@@ -9,13 +9,13 @@ import requests
 url = "https://so.gushiwen.org/mingju/"
 req = requests.get(url)
 soup = bs4.BeautifulSoup(req.text, "html.parser")
-lst = soup.find_all("a", {"target":"_blank"})
-with open('mingju.txt', 'w') as f:
+lst = soup.find_all("a", {"target": "_blank"})
+with open('mingju.txt', 'w', encoding='utf-8') as f:
     for i in range(len(lst) - 1):
         s = str(lst[i + 1])
         res = s[s.find("\">") + 2: s.find("</")]
         f.write(res)
-        if i%2 == 0:
+        if i % 2 == 0:
             f.write('---')
         else:
             f.write('\n')
