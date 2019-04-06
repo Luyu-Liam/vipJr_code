403f940808de3389f441a9e62e7d18ff3fcafc32
liamlycoder <luyu.real@qq.com>
2019-04-06 19:50:16 +0800

    modify qr01

--- MyQR/qr01.py
+++ MyQR/qr01.py
@@ -62,13 +62,6 @@ def get_cci(ver, mode, string):
     return cci
 
 
-mode_indicator = {
-    'numeric': '0001',
-    'alphanumeric': '0010',
-    'byte': '0100',
-    'kanji': '1000'
-}
-
 mode_encoding = {
     'numeric': numeric_encoding,
     'alphanumeric': alphanumeric_encoding,
